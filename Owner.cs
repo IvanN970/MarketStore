8e01f4e448e3b8f7be27fe1c96a77df591536a23
IvanN970 <73254039+IvanN970@users.noreply.github.com>
2020-10-29 11:31:18 +0200

    Add files via upload

---
+++ Owner.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+namespace MarketStore
+{
+    public class Owner
+    {
+        private string name;
+        private string town;
+        private int age;
+        public Owner()
+        {
+            name = null;
+            town = null;
+            age = 0;
+        }
+        public string Name
+        {
+            set { name = value;}
+            get { return name;}
+        }
+        public string Town
+        {
+            set { town = value;}
+            get { return town;}
+        }
+        public int Age
+        {
+            set { age = value;}
+            get { return age;}
+        }
+        public override string ToString()
+        {
+            return "Name:" + " " + name + " " + "Town:" + " " + town + " " + "Age:" + " " + age;
+        }
+    }
+}
