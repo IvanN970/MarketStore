95497f6e593989eee7c078e0cd6059a8e55c5743
IvanN970 <73254039+IvanN970@users.noreply.github.com>
2020-10-29 11:30:37 +0200

    Add files via upload

---
+++ DiscountCard.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+namespace MarketStore
+{
+    public abstract class DiscountCard
+    {
+        protected Owner owner;
+        protected double turnover;
+        protected double discountRate;
+        public DiscountCard()
+        {
+            owner = null;
+            turnover = 0;
+        }
+        public Owner Owner
+        {
+            set { owner = value;}
+            get { return owner; }
+        }
+        public double Turnover
+        {
+            set { turnover = value;}
+            get { return turnover;}
+        }
+        public double DiscountRate
+        {
+            set { discountRate = value;}
+            get { return discountRate;}
+        }
+        public abstract void CalculateDiscountRate();
+    }
+}
