244911d54abe3a5fa5f77256093843b57f9d8312
IvanN970 <73254039+IvanN970@users.noreply.github.com>
2020-10-29 11:34:37 +0200

    Add files via upload

---
+++ BronzeCard.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+namespace MarketStore
+{
+    public class BronzeCard:DiscountCard
+    {
+        public BronzeCard() : base()
+        {
+            discountRate = 0;
+        }
+        public override void CalculateDiscountRate()
+        {
+            if(Turnover>=100 && Turnover<=300)
+            {
+                discountRate = 1;
+            }
+            else if(Turnover>300)
+            {
+                discountRate = 2.5;
+            }
+            else
+            {
+                Console.WriteLine("No discount for purchase under 100$");
+            }
+        }
+    }
+}
