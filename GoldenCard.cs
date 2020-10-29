fc5cafea5287a6d744c462e8ae1110a7b835f091
IvanN970 <73254039+IvanN970@users.noreply.github.com>
2020-10-29 11:31:56 +0200

    Add files via upload

---
+++ GoldenCard.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+namespace MarketStore
+{
+    public class GoldenCard:DiscountCard
+    {
+        public GoldenCard() : base()
+        {
+            discountRate = 2;
+        }
+        public override void CalculateDiscountRate()
+        {
+            int count = 0;
+            while(count<Turnover)
+            {
+                discountRate += 1;
+                count += 100;
+                if(discountRate==10)
+                {
+                    break;
+                }
+            }
+        }
+    }
+}
