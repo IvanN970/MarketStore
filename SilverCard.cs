b7b44262102102dd367fca98dca36b18b168e38a
IvanN970 <73254039+IvanN970@users.noreply.github.com>
2020-10-29 11:33:45 +0200

    Add files via upload

---
+++ SilverCard.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+namespace MarketStore
+{
+    public class SilverCard:DiscountCard
+    {
+        public SilverCard() : base()
+        {
+            discountRate = 2;
+        }
+        public override void CalculateDiscountRate()
+        {
+            if(Turnover>300)
+            {
+                DiscountRate = 3.5;
+            }
+        }
+    }
+}
